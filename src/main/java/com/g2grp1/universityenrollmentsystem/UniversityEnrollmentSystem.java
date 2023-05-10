63282074403d87e6d22bf7dedb47cb37815bd5e7
Nathaniel Tampus <contact@nize.ph>
2023-05-10 19:43:35 +0800

    init

---
+++ src/main/java/com/g2grp1/universityenrollmentsystem/UniversityEnrollmentSystem.java
@@ -0,0 +1,8 @@
+package com.g2grp1.universityenrollmentsystem;
+
+public class UniversityEnrollmentSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
